8a5f01fa33778bf6aa5baa537a76489f64be2ea0
fajriiqbal <48725485+fajriiqbal@users.noreply.github.com>
2025-09-11 07:27:36 -0500

    aseets

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PMB-MTs Sunan Kalijaga</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="/assets/LOGOMADA.png"></script>
     <style>
         /* Custom mobile-app like styling */
         @media (max-width: 768px) {
