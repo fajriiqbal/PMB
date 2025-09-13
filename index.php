32ec0fa26682d6453c3f1f73190506a0fb951b5c
fajriiqbal <48725485+fajriiqbal@users.noreply.github.com>
2025-09-13 03:01:26 -0500

    edit fotter

--- index.php
+++ index.php
@@ -348,7 +348,7 @@
 
 
         <!-- Footer -->
-        <footer class="bg-gray-800 text-white py-10">
+        <!-- <footer class="bg-gray-800 text-white py-10">
             <div class="container mx-auto px-4">
                 <div class="flex flex-col md:flex-row justify-between items-center">
                     <div class="mb-6 md:mb-0">
@@ -371,7 +371,7 @@
                     <p>© 2025 <?php echo $schoolName; ?>. IT Madsuka.</p>
                 </div>
             </div>
-        </footer>
+        </footer> -->
 
         <!-- Floating Register Button for Mobile -->
         <?php if ($isMobile): ?>
