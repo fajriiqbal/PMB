0937e14b41b75b5bcae9c0c6b04ce2de01deb4e7
fajriiqbal <48725485+fajriiqbal@users.noreply.github.com>
2025-09-18 22:52:33 +0700

    gender edut

--- index.php
+++ index.php
@@ -507,7 +507,7 @@ async function loadStats() {
         total++;
 
         // hitung gender
-        if (gender === "laki-laki") male++;
+        if (gender === "Laki-Laki") male++;
         else if (gender === "perempuan") female++;
 
         // hitung pondok
