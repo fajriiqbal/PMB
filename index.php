d7eb77402fa8d9345d80194ee817d5c2ad995734
fajriiqbal <48725485+fajriiqbal@users.noreply.github.com>
2025-10-22 20:26:10 +0700

    editt

--- index.php
+++ index.php
@@ -481,7 +481,7 @@ function renderTable(data) {
           <td class="border px-4 py-2">
                     ${hp ? `<a href="${linkWA}" target="_blank" class="bg-green-500 text-white px-3 py-1 rounded" onclick="markContacted('${hp}')">Hubungi</a>` : "-"} ${contacted}
                 </td>
-        `;
+        `
         
         tbody.appendChild(tr);
     });
