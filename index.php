907da2eee9314120dbfbebc728eaffa5b8561c73
fajriiqbal <48725485+fajriiqbal@users.noreply.github.com>
2025-11-30 01:43:45 +0700

    d

--- index.php
+++ index.php
@@ -219,13 +219,12 @@ nav.bottom-nav a.active{color:var(--accent);font-weight:700}
   </a>
 
   <a href="#" id="navData" onclick="scrollToSection('pendaftar');return false;">
-    <!-- Data Table Icon -->
+    <!-- Table Icon -->
     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
       <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 10h16M4 14h16M4 18h16"/>
     </svg>
     <div>Data</div>
   </a>
-
 
   <a href="#" id="navMore" onclick="confirmDownload();return false;">
     <!-- Download Icon -->
@@ -234,8 +233,6 @@ nav.bottom-nav a.active{color:var(--accent);font-weight:700}
     </svg>
     <div>Download</div>
   </a>
-
-
 </nav>
 
 <!-- Modal Konfirmasi Download -->
